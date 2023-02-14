92404fa0f6870368574c0828487b7c436bdedbcc
jousemarquez <mquezflores@gmail.com>
2023-02-14 18:24:12 +0100

    Cambios de estilo

--- js/additions.js
+++ js/additions.js
@@ -4,3 +4,8 @@ function onClick() {
     clicks += 1;
     document.getElementById("love").innerHTML = clicks;
 };
+
+function ocultarEjercicio1() {
+    let div = document.querySelector("#explain");
+    div.innerHTML = ``;
+}
