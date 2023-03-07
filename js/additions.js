501f1fabc80683f4137651ad1fd10b2508e77367
jousemarquez <mquezflores@gmail.com>
2023-03-07 13:38:16 +0100

    Actualización botón Share on twitter

--- js/additions.js
+++ js/additions.js
@@ -4,3 +4,17 @@ function onClick() {
     clicks += 1;
     document.getElementById("love").innerHTML = clicks;
 };
+
+function tweet() {
+    // URL de la página actual
+    var url = window.location.href;
+    
+    // Texto a tuitear (puedes cambiarlo por el contenido que quieras)
+    var text = "Can't believe what I found about me here. Did your tried?⬇️\n\n" + url + " \n\n@vedruna";
+    
+    // URL del botón para compartir en Twitter
+    var twitterUrl = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(text);
+    
+    // Abrir ventana emergente para compartir en Twitter
+    window.open(twitterUrl);
+};
